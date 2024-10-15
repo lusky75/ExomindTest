38c575633a27610b3e04188cd5e1a3d47018945f
Lucas CHEN <lusky75@Ordinateur-portable-de-Lucas.local>
2024-10-15 15:28:54 +0200

    Created the base of the app

---
+++ ExomindTest/ExomindTest/ExomindTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExomindTestApp.swift
+//  ExomindTest
+//
+//  Created by Lucas CHEN on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExomindTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
